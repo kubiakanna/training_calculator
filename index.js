1ba18f2f1bd38183db78020f9b9cd272b197e538
annakubiak <89855163+kubiakanna@users.noreply.github.com>
2022-02-10 17:54:06 +0100

    Add different operators using if...else

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const readline = require('readline-sync');
 
+console.log('Welcome to the calculator!');
+console.log('==========================');
+
+console.log('Please enter one of the following operators: +, -, * or /: ');
+const operatorInput = readline.prompt();
+
 console.log('Please enter the first number: ');
 const firstNumber = readline.prompt();
 
 console.log('Please enter the second number: ');
 const secondNumber = readline.prompt();
 
-const result = +firstNumber * +secondNumber;
-!isNaN(result) ? console.log(result) : console.log('Invalid input.');
+let result;
+
+if (operatorInput === '+') {
+ result = +firstNumber + +secondNumber;
+} else if (operatorInput === '-') {
+    result = +firstNumber - +secondNumber;
+} else if (operatorInput === '*') {
+    result = +firstNumber * +secondNumber;
+} else if (operatorInput === '/') {
+    result = +firstNumber / +secondNumber;
+}
+
+!isNaN(result) ? console.log('The result is: ' + result) : console.log('Invalid input.');
