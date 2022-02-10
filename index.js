7eef5fcada89eff797c9d99eeac4cf6c1128163a
annakubiak <89855163+kubiakanna@users.noreply.github.com>
2022-02-10 11:06:48 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Welcome!');
+const readline = require('readline-sync');
+
+console.log('Please enter the first number: ');
+const firstNumber = readline.prompt();
+
+console.log('Please enter the second number: ');
+const secondNumber = readline.prompt();
+
+const result = +firstNumber * +secondNumber;
+result ? console.log(result) : console.log('Invalid input.');
