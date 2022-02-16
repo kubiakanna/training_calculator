1f193fdf822c37d30b34ebd2f62bb4f5c9bb005f
annakubiak <89855163+kubiakanna@users.noreply.github.com>
2022-02-16 17:12:37 +0100

    Refactor the code to use functions and force valid numbers

--- index.js
+++ index.js
@@ -1,49 +1,75 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to the calculator!');
-console.log('==========================');
+const possibleOperators = ['+', '-', '*', '/'];
 
-console.log('Please enter one of the following operators: +, -, * or /: ');
-let operatorInput = readline.prompt();
-
-console.log('How many numbers do you want to operate on? ');
-let howMany = readline.prompt();
-
-let arrNumbers = [];
-
-for (let i = 0; i < howMany; i++) {
-    console.log(`Please enter number ${i+1}: `);
-    arrNumbers[i] = +readline.prompt();
+const printWelcomeMessage = () => {
+    console.log('Welcome to the calculator!');
+    console.log('==========================');
 }
 
-let result;
-
-switch (operatorInput) {
-    case '+':
-        result = arrNumbers.reduce((acc, curr) => {
-            return acc + curr;
-        });
-        break;
-     
-    case '-':
-        result = arrNumbers.reduce((acc, curr) => {
-            return acc - curr;
-        });
-        break;
-     
-    case '*':
-        result = arrNumbers.reduce((acc, curr) => {
-            return acc * curr;
-        });
-        break;
-     
-    case '/':
-        result = arrNumbers.reduce((acc, curr) => {
-            return acc / curr;
-        });
-        break;
-
-    default:
-        break;
+const getInfo = (message) => {
+    console.log(message);
+    const userInput = readline.prompt();
+    return userInput;
 }
-!isNaN(result) ? console.log('The result is: ' + result) : console.log('Invalid input.');
+
+const performCalculations = (operator, numbers) => {
+    let result;
+    switch (operator) {
+        case '+':
+            result = numbers.reduce((acc, curr) => {
+                return acc + curr;
+            });
+            break;
+
+        case '-':
+            result = numbers.reduce((acc, curr) => {
+                return acc - curr;
+            });
+            break;
+
+        case '*':
+            result = numbers.reduce((acc, curr) => {
+                return acc * curr;
+            });
+            break;
+
+        case '/':
+            result = numbers.reduce((acc, curr) => {
+                return acc / curr;
+            });
+            break;
+
+        default:
+            break;
+    }
+    console.log('The result is: ' + result);
+}
+
+printWelcomeMessage();
+
+let runAgain = 'Y';
+while (runAgain.toUpperCase() === 'Y') {
+
+    let operatorInput = getInfo('Please enter one of the following operators: +, -, * or /: ');
+    while (!possibleOperators.includes(operatorInput)) {
+        operatorInput = getInfo('Incorrect input. Please enter one of the following operators: +, -, * or /: ');
+    }
+
+    let howMany = parseFloat(getInfo('How many numbers do you want to operate on? '));
+    while (isNaN(howMany) || !Number.isInteger(howMany)) {
+        howMany = parseFloat(getInfo('Incorrect input. How many numbers do you want to operate on? '));
+    }
+
+    let arrNumbers = [];
+    for (let i = 0; i < howMany; i++) {
+        arrNumbers[i] = parseFloat(getInfo(`Please enter number ${i + 1}: `));
+        while (isNaN(arrNumbers[i])) {
+            arrNumbers[i] = parseFloat(getInfo(`Incorrect input. Please enter number ${i + 1}: `));
+        }
+    }
+
+    performCalculations(operatorInput, arrNumbers);
+
+    runAgain = getInfo('If you want to calculate again type Y, to exit type anything else. ');
+}
