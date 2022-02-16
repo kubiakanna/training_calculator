01bf84b473e1c00aa932a11ba775c7b1b7c64007
annakubiak <89855163+kubiakanna@users.noreply.github.com>
2022-02-16 17:39:00 +0100

    Divide the code into smaller functions

--- index.js
+++ index.js
@@ -7,10 +7,38 @@ const printWelcomeMessage = () => {
     console.log('==========================');
 }
 
+
 const getInfo = (message) => {
     console.log(message);
     const userInput = readline.prompt();
     return userInput;
+}
+
+const getOperator = () => {
+    let operatorInput = getInfo('Please enter one of the following operators: +, -, * or /: ');
+    while (!possibleOperators.includes(operatorInput)) {
+        operatorInput = getInfo('Incorrect input. Please enter one of the following operators: +, -, * or /: ');
+    }
+    return operatorInput;
+}
+
+const howManyNumbers = () => {
+    let howMany = parseFloat(getInfo('How many numbers do you want to operate on? '));
+    while (isNaN(howMany) || !Number.isInteger(howMany)) {
+        howMany = parseFloat(getInfo('Incorrect input. How many numbers do you want to operate on? '));
+    }
+    return howMany;
+}
+
+const getArrOfNumbers = (size) => {
+    let arrNumbers = [];
+    for (let i = 0; i < size; i++) {
+        arrNumbers[i] = parseFloat(getInfo(`Please enter number ${i + 1}: `));
+        while (isNaN(arrNumbers[i])) {
+            arrNumbers[i] = parseFloat(getInfo(`Incorrect input. Please enter number ${i + 1}: `));
+        }
+    }
+    return arrNumbers;
 }
 
 const performCalculations = (operator, numbers) => {
@@ -43,33 +71,23 @@ const performCalculations = (operator, numbers) => {
         default:
             break;
     }
-    console.log('The result is: ' + result);
+    return result;
+}
+
+const performOneCalculation = () => {
+    const operator = getOperator();
+    const howMany = howManyNumbers();
+    const arrNumbers = getArrOfNumbers(howMany);
+    const answer = performCalculations(operator, arrNumbers);
+
+    console.log('The result is: ' + answer);
 }
 
 printWelcomeMessage();
 
 let runAgain = 'Y';
 while (runAgain.toUpperCase() === 'Y') {
-
-    let operatorInput = getInfo('Please enter one of the following operators: +, -, * or /: ');
-    while (!possibleOperators.includes(operatorInput)) {
-        operatorInput = getInfo('Incorrect input. Please enter one of the following operators: +, -, * or /: ');
-    }
-
-    let howMany = parseFloat(getInfo('How many numbers do you want to operate on? '));
-    while (isNaN(howMany) || !Number.isInteger(howMany)) {
-        howMany = parseFloat(getInfo('Incorrect input. How many numbers do you want to operate on? '));
-    }
-
-    let arrNumbers = [];
-    for (let i = 0; i < howMany; i++) {
-        arrNumbers[i] = parseFloat(getInfo(`Please enter number ${i + 1}: `));
-        while (isNaN(arrNumbers[i])) {
-            arrNumbers[i] = parseFloat(getInfo(`Incorrect input. Please enter number ${i + 1}: `));
-        }
-    }
-
-    performCalculations(operatorInput, arrNumbers);
+    performOneCalculation();
 
     runAgain = getInfo('If you want to calculate again type Y, to exit type anything else. ');
 }
